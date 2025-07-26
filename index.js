93a303b59e5ce36151717b96d9fda74143190aa1
RAHUL SHARMA <rahulsharma@RAHULs-MacBook-Air-2.local>
2025-07-26 12:18:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// First Approach
+    // const {add, PI} = require("./math");
+    // const {square} = require("./math");
+
+    // let result = add(90, 100);
+    // console.log(result);
+    // console.log("The value of PI: ", PI);
+    // console.log(square(100));
+
+// second approach
+    // const math = require("./math");
+    // console.log(math.PI);
+    // console.log(math.add(90, 90));
+
+// const everything = require("./math");
+
+// console.log(everything)
---
+++ math.js
@@ -0,0 +1,48 @@
+// First Approach:
+    // const PI = 3.14
+
+    // function add(a, b){
+    //     return a+b;
+    // }
+    // function square(a){
+    //     return a*a;
+    // }
+
+    // exports.add = add;
+    // exports.PI = PI;
+    // exports.square = square;
+
+
+// second Approach:
+
+        // exports.PI = 3.14;
+
+        // exports.add = function(a, b){
+        //     return a+b;
+        // }
+        // exports.square = (a)=>{
+        //     return a*a;
+        // }
+
+
+    // Third approach
+
+
+        // const PI = 3.14;
+
+        // function add(a, b){
+        //     return a+b;
+        // }
+        // function square(a){
+        //     return a*a;
+        // }
+
+        // module.exports = {PI, add, square};
+
+
+    
+exports.PI = 3.14;
+module.exports = ()=>{
+    console.log("Hello from the default function of the math module!");
+}
+
